90b4b165887ce1e43edf83397c64951b4eada851
Denzhel Distor <denzhelroyceian@gmail.com>
2020-03-20 17:12:58 -0500

    Added customer inserts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# frozen_string_literal: true
+
+Customer.destroy_all
+
+NUMBER_OF_CUSTOMERS = 5
+
+NUMBER_OF_CUSTOMERS.times do |index|
+  puts index
+  if index < 3
+    Customer.create(
+      full_name: Faker::Name.unique.name,
+      phone_number: Faker::PhoneNumber.unique.phone_number,
+      email_address: Faker::Internet.unique.email,
+      notes: Faker::Lorem.paragraph
+    )
+  else
+    Customer.create(
+      full_name: 'Faker::Name.unique.name',
+      phone_number: Faker::PhoneNumber.unique.phone_number,
+      email_address: '',
+      notes: Faker::Lorem.paragraph
+    )
+  end
+end
+
+puts "Created #{Customer.count} Customers."
+
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
