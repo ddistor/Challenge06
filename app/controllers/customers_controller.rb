8927bc05c2a0a92040ef85c7edb67075094e8abe
Denzhel Distor <denzhelroyceian@gmail.com>
2020-03-20 21:16:22 -0500

    Added title to customer index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: [nil, ''])
+  end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
 end
